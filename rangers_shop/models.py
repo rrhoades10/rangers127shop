b8ef6615a67264def852cdba04b25a691f99690c
rrhoades10 <rrhoades10@yahoo.com>
2023-09-28 13:01:54 -0500

    pleeeeease

--- rangers_shop/models.py
+++ rangers_shop/models.py
@@ -210,7 +210,7 @@ class ProductSchema(ma.Schema):
 
 
 product_schema = ProductSchema() #this is for passing 1 singular product 
-product_schemas = ProductSchema(many = True) #this for passing multiple products, list of dictionaries 
+products_schema = ProductSchema(many = True) #this for passing multiple products, list of dictionaries 
     
 
     
