67aa5ed70b543af2df391d5fa476a613b4e74040
rrhoades10 <rrhoades10@yahoo.com>
2023-09-28 11:33:25 -0500

    sdf

--- rangers_shop/blueprints/api/routes.py
+++ rangers_shop/blueprints/api/routes.py
@@ -1,157 +1,157 @@
 from flask import Blueprint, request, jsonify 
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity 
-
-#internal imports 
-from rangers_shop.models import Customer, Product, ProdOrder, Order, db, product_schema, products_schema 
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from ecommerce_app.models import db, Customer, ProdOrder, Order,  Product, product_schema, product_schemas
 
 
-
-#instantiate our blueprint
-api = Blueprint('api', __name__, url_prefix = '/api') #all of our endpoints need to be prefixed with /api
-
+api = Blueprint('api', __name__, url_prefix='/api')
 
 @api.route('/token', methods = ['GET', 'POST'])
 def token():
 
     data = request.json
-
-    if data:
-        client_id = data['client_id'] #looking for the key of client_id on the dictionary passed to us
-        access_token = create_access_token(identity=client_id) 
+    if data: 
+        client_id = request.json['client_id']
+        access_token = create_access_token(identity=client_id)
         return {
-            'status' : 200,
-            'access_token' : access_token 
+            'status': 200,
+            'access_token': access_token
         }
-    
     else:
         return {
             'status': 400,
             'message': 'Missing Client Id. Try Again'
         }
-    
 
-#creating our READ data request for shop
+
 @api.route('/shop')
 @jwt_required()
 def get_shop():
 
-    shop = Product.query.all() #list of objects, we can't send a list of objects through api calls 
+    shop = Product.query.all()
 
-    response = products_schema.dump(shop) #takes our list of objects and turns it into a list of dictionaries 
-    return jsonify(response) #jsonify essentially stringifies the list to send to our frontend 
+    response = product_schemas.dump(shop)
+    return jsonify(response)
 
 
-#creating our READ data request for orders READ associated with 'GET' 
-@api.route('/order/<cust_id>')
+@api.route('/order/<cust_id>', methods = ['GET'])
 @jwt_required()
-def get_order(cust_id):
+def get_orders(cust_id):
+    
 
+    #need to grab all the orders made by each customer, and all the products associated with each order 
+    #first grab all the diferent order_ids for uses
+    #then grab the individual products per the orders 
 
-    #We need to grab all the order_ids associated with the customer
-    #Grab all the products on that particular order 
+    prodord = ProdOrder.query.filter(ProdOrder.cust_id == cust_id).all()
 
-    prodorder = ProdOrder.query.filter(ProdOrder.cust_id == cust_id).all()
-
-
+    # data = {}
     data = []
+    # id = 1
 
+    #we need to traverse the prodord to get all the individual orders for that customer
+    for order in prodord:
 
-    #need to traverse to grab all the products from each order 
-    for order in prodorder: 
+        # if order.order_id not in data:
+        #     data[order.order_id] = []
 
-        #order is the prodorder object so has a prod_id associated with it
-
-
+        print(order.quantity)
+            
+        #query the Product object associated with the product_id 
         product = Product.query.filter(Product.prod_id == order.prod_id).first()
 
-        prod_data = product_schema.dump(product) #change this from an object to a dictionary 
+        #use our schema to jsonify our product &
+        prod_data = product_schema.dump(product)
+    
+        #for the product quantity on order not the total quantity available 
+        prod_data['quantity'] = order.quantity
+        prod_data['order_id'] = order.order_id
+        prod_data['id'] = order.prodord_id
+        # id += 1
 
-        prod_data['quantity'] = order.quantity #coming from the prodorder table 
-        prod_data['order_id'] = order.order_id #want to associate this product with a specific order 
-        prod_data['id'] = order.prodorder_id #need to make products unique even if they are the same product 
-
+        #add quantity attribute to that product before putting it in our list
+        # product.quantity = order.quantity
+        # data[order.order_id].append(prod_data)
         data.append(prod_data)
 
 
-    return jsonify(data)
+
+    return data 
 
 
 
-#create our CREATE data request for orders, usually associated with 'POST' 
-@api.route('/order/create/<cust_id>', methods = ['POST'])
+
+@api.route('/order/<cust_id>', methods = ['POST'])
 @jwt_required()
 def create_order(cust_id):
-
     data = request.json
+    print(data)
 
-    customer_order = data['order']
-    print(customer_order)
+    cust_order  = data['order']
 
     customer = Customer.query.filter(Customer.cust_id == cust_id).first()
     if not customer:
         customer = Customer(cust_id)
         db.session.add(customer)
 
+
     order = Order()
     db.session.add(order)
 
-    #looping through the customer order list of dictionaries for each product
-    for product in customer_order:
+    for product in cust_order:
+        print(product)
+        prodord = ProdOrder(product['prod_id'], product['quantity'], product['price'], order.order_id, customer.cust_id)
+        db.session.add(prodord)
 
-        prodorder = ProdOrder(product['prod_id'], product['quantity'], product['price'], order.order_id, customer.cust_id)
-        db.session.add(prodorder)
+        #adds price from prodord (which multiples quantity by price) to order total
+        order.increment_order_total(prodord.price)
 
-        #add price from our prodorder table to increment our total order price
-        order.increment_order_total(prodorder.price)
-
-
-        #decrement the available amount of that specific product in our shop
-        current_product = Product.query.filter(Product.prod_id == product['prod_id']).first()
+        #decrements total quantity of product when bought by customer 
+        current_product = Product.query.get(product['prod_id'])
         current_product.decrement_quantity(product['quantity'])
-
 
     db.session.commit()
 
-
     return {
         'status': 200,
-        'message': 'New Order was created!'
+        'message': 'new order was created!'
     }
 
 
-#create our UPDATE route for our order, usually associated with 'PUT' 
-
-@api.route('/order/update/<order_id>', methods = ['PUT', 'POST']) 
+@api.route('/order/<order_id>', methods = ['PUT'])
 @jwt_required()
 def update_order(order_id):
-
-    # try: 
 
     data = request.json
     new_quantity = int(data['quantity'])
     prod_id = data['prod_id']
 
+    print(prod_id)
+    print(new_quantity)
+    
+
 
     prodorder = ProdOrder.query.filter(ProdOrder.order_id == order_id, ProdOrder.prod_id == prod_id).first()
-    order = Order.query.get(order_id) #.get() is specific for ids 
+    order = Order.query.get(order_id)
     product = Product.query.get(prod_id)
 
 
-    #update the product price based on the new quantity
+    #sets the new quantity of that product for this specific order and the total cost of that product for the order
+    
+    
     prodorder.set_price(product.price, new_quantity)
 
-    diff = abs(prodorder.quantity - new_quantity)
+    diff = abs(new_quantity - prodorder.quantity)
+    print("diff", diff)
 
-    #based on if the new quantity is higher or lower we either new to decrement or increment total product quantity & order cost 
+    #if our new quantity is less than the old, we can add back to our product quantity & decrement our order $ total
+    if prodorder.quantity > new_quantity:
+        product.increment_quantity(diff)
+        order.decrement_order_total(prodorder.price)
 
-    if prodorder.quantity < new_quantity: 
-        product.decrement_quantity(diff) #decrease our available inventory
-        order.increment_order_total(prodorder.price) #our order total is going to be more 
-
-    elif prodorder.quantity > new_quantity:
-        product.increment_quantity(diff) #increase our available inventory
-        order.decrement_order_total(prodorder.price) #our order total is going to be less 
-
+    #if our new quantity is more than the old, we need to take away product quantity & incrmeent our order $ total
+    elif prodorder.quantity < new_quantity:
+        product.decrement_quantity(diff)
+        order.increment_order_total(prodorder.price)
 
     prodorder.update_quantity(new_quantity)
 
@@ -159,14 +159,39 @@ def update_order(order_id):
 
     return {
         'status': 200,
-        'message': 'Order was successfully updated!'
+        'message': 'order was updated!'
     }
+
+
+
+@api.route('/order/<order_id>', methods = ['DELETE'])
+@jwt_required()
+def delete_item_order(order_id):
+
     
-    # except:
+    data = request.json
+    print('data', data)
+    prod_id = data['prod_id']
+    # print(prod_id)
+    # print(order_id)
 
-    #     return {
-    #         'status': 400,
-    #         'message': 'Unable to process your request. Please try again!'
-    #     }
+    prodorder = ProdOrder.query.filter(ProdOrder.order_id == order_id, ProdOrder.prod_id == prod_id).first()
+    # print(prodorder)
+    order = Order.query.get(order_id)
+    product = Product.query.get(prod_id)
+
+    # print(order)
+    # print(prodorder.price)
+    order.decrement_order_total(prodorder.price)
+    product.increment_quantity(prodorder.quantity)
+
+    
+
+    db.session.delete(prodorder)
+    db.session.commit()
 
 
+    return {
+        'status': 200,
+        'message': 'order was deleted!'
+    } 
